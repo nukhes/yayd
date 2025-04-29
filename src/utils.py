69e0e6ce44385e32f99011ab0bc92bd6ccb8c65e
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2025-04-29 18:06:30 -0300

    feat: downloads videos in the user dir

--- src/utils.py
+++ src/utils.py
@@ -1,17 +1,20 @@
 import os
 import yt_dlp
-import platform
 from pathlib import Path
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def get_download_folder():
 	"""Return user download dir"""
-	system = platform.system()
-	if system == "Windows":
+	if  os.name == "nt":
 		return os.path.join(os.environ["USERPROFILE"], "Downloads")
 	else:
 		return os.path.join(Path.home(), "Downloads")
 
 def download_task(opts, url):
 	"""Start a download task"""
+	clear_screen()
+	print(f"Starting download (quality: {opts['format']})")
 	with yt_dlp.YoutubeDL(opts) as task:
 		task.download([url])
